817280b14c6f76f24ebdbbd63d0be2cfdd309f3d
Julio Adrian Ferreyra Tello <julioferreyra.dev@gmail.com>
2025-11-11 23:36:37 -0600

    added custom svg files, todo: please add the remaining svg files

--- ejidonoticias_application/noticias/views.py
+++ ejidonoticias_application/noticias/views.py
@@ -3,28 +3,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Noticia
 
-blogs : List[dict[str,Any]] = [
-    {"title": "Esta es mi casa",
-        "author": "Me mismo",
-        "date": "Hoy",
-        "reading_time": "5 minutos",
-        "views": 1000,
-        "category": "Villa",
-        "tags": ["hogar", "familia", "comodidad"],
-        "slug": "villa-casa",
-        "banner_image_url": "https://placehold.co/1920x927?text=Villa+Casa",},
-    {
-        "title": "Esta es mi casa 2",
-        "author": "Me mismo",
-        "date": "Hoy",
-        "reading_time": "5 minutos",
-        "views": 2500,
-        "category": "Mundo",
-        "tags": ["hogar", "familia", "comodidad"],
-        "slug": "villa-hogar",
-        "banner_image_url": "https://placehold.co/1920x927?text=Hello+World",
-    }
-]
 
 
 def index(request) -> HttpResponse:
