4d3a7a6e7ade809db9e561fc278870db5ea6c304
Julio Adrian Ferreyra Tello <julioferreyra.dev@gmail.com>
2025-11-13 15:03:05 -0600

    added 404 not found exception

--- ejidonoticias_application/noticias/urls.py
+++ ejidonoticias_application/noticias/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns: list[URLPattern] = [
     path(route='', view=views.index, name='index'),
     path(route='blog/<str:slug>/', view=views.blog, name='blog'),
+    path(route='404/', view=views.not_found, name='not_found'),
     
 ]
--- ejidonoticias_application/noticias/views.py
+++ ejidonoticias_application/noticias/views.py
@@ -1,5 +1,5 @@
 from typing import Any, List
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
 from .models import Noticia
 
@@ -11,9 +11,15 @@ def index(request) -> HttpResponse:
     return render(request, template_name='index.html', context={'content': noticias, "page_title": 'Inicio'})
 
 def blog(request, slug:str) -> HttpResponse:
-    blog = Noticia.objects.get(slug=slug)
+    blog = get_object_or_404(Noticia, slug=slug)
+    if blog is None:
+        return redirect(to='not_found')
+        
     blog.vistas += 1
     blog.save()
     return render(request, template_name='screens/blog.html', context={'blog': blog})
 
+
+def not_found(request) -> HttpResponse:
+    return render(request, template_name='screens/404.html', context={})
 # Create your views here.
